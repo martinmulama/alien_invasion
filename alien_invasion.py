488e4e65d92084d859253e274ed1e2d81cff47c2
martinmulama <martin.mulama@yahoo.com>
2025-08-03 12:29:04 +0300

    Modified ship movement so it doesn't dissapear at the edges of the screen. Refactored the check event methods into key_up and key_down methods. Also added ability to quit with Q.

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,27 +16,35 @@ class AlienInvasion:
 		self.bg_color = (self.settings.bg_color)
 		self.ship = Ship(self)
 
+	def _check_keydown_events(self, event):
+		""" Respond to keypresses """
+		if event.key == pygame.K_RIGHT:
+			# Move the ship to the right
+			self.ship.moving_right = True
+		elif event.key == pygame.K_LEFT:
+			# Move the ship to the left
+			self.ship.moving_left = True
+		elif event.key == pygame.K_q:
+			sys.exit()
+
+	def _check_keyup_events(self, event):
+		""" Respond to key releases """
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = False
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = False
+
 	def _check_events(self):
 		""" Respond to keypresses and mouse events"""
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
 			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RIGHT:
-					# Move the ship to the right
-					self.ship.moving_right = True
-				elif event.key == pygame.K_LEFT:
-					# Move the ship to the left
-					self.ship.moving_left = True
+				self._check_keydown_events(event)	
 			elif event.type == pygame.KEYUP:
 				# Stop movement when direction is released
-				if event.key == pygame.K_RIGHT:
-					self.ship.moving_right = False
-				elif event.key == pygame.K_LEFT:
-					self.ship.moving_left = False
-			
-
-
+				self._check_keyup_events(event)
+	
 	def _update_screen(self):
 		"""Updates images on the screen and flip to the new screen"""
 		self.screen.fill(self.settings.bg_color)
