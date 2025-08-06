52fadcb5b4a5be27f4ac246f3b898468b86acdf6
martinmulama <martin.mulama@yahoo.com>
2025-08-06 15:12:10 +0300

    Made the direction of alien fleet mobile. When they reach the edge of the screen they drop and start moving in the opposite direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,20 @@ class AlienInvasion:
 		alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
 		self.aliens.add(alien)
 
+	def _check_fleet_edges(self):
+		"""Respond appropriately if any aliens have reached an edge."""
+		for alien in self.aliens.sprites():
+			if alien.check_edges():
+				self._change_fleet_direction()
+				break
+
+	def _change_fleet_direction(self):
+		"""Drop the entire fleet and change the fleet's direction."""
+		for alien in self.aliens.sprites():
+			alien.rect.y += self.settings.fleet_drop_speed
+			self.settings.fleet_direction *= -1
+
+
 	def _update_bullets(self):
 		""" Update position of bullets and get rid of old bullets """
 		# Update bullet positions
@@ -87,7 +101,12 @@ class AlienInvasion:
 		#Get rid of bullets that have dissapeared.
 		for bullet in self.bullets.copy():
 				if bullet.rect.bottom <= 0:
-					self.bullets.remove(bullet)	
+					self.bullets.remove(bullet)
+
+	def _update_aliens(self):
+		"""	Check if fleet is at an edge then Update the position of all aliens in the fleet """
+		self._check_fleet_edges()
+		self.aliens.update()
 
 	def _update_screen(self):
 		"""Updates images on the screen and flip to the new screen"""
@@ -106,7 +125,8 @@ class AlienInvasion:
 		while True:
 			self._check_events()
 			self.ship.update()
-			self._update_bullets()	
+			self._update_bullets()
+			self._update_aliens()	
 			self._update_screen()
 
 	def _fire_bullet(self):
